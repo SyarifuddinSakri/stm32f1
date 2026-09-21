ef3f5f2c5e0545b33ba54b8ce6fb1edcc83b3567
syarifuddin <syarifuddinsakri@gmail.com>
2026-09-22 00:14:31 +0800

    removed example file of using HTTP parser and put it in utc_clock/main.c file

--- utc_clock/main.c
+++ utc_clock/main.c
@@ -5,6 +5,18 @@
 #include "esp8266.h"
 #include <libopencm3/stm32/flash.h>
 #include <libopencm3/stm32/rcc.h>
+#include <string.h>
+
+static void on_http_request(uint8_t link_id, const char *request,
+                            uint16_t len) {
+  (void)len;
+  if (strncmp(request, "GET / ", 6) == 0) {
+    esp8266_http_send_response(link_id, 200, "text/html",
+                               "<h1>Hello from STM32</h1>");
+  } else {
+    esp8266_http_send_response(link_id, 404, "text/plain", "Not Found");
+  }
+}
 
 int main(void) {
   /* add your own code */
@@ -13,6 +25,11 @@ int main(void) {
   esp8266_setup();
   esp8266_check();
   esp8266_connect("NabilaSubri_5G", "19971109@");
+  esp8266_set_mode(ESP8266_MODE_CLIENT_HOST);            // CWMODE=3
+  esp8266_connect("HomeWiFi", "homepassword");           // join home AP (STA)
+  esp8266_set_ap_config("MyESP32", "esp8266pass", 6, 3); // WPA2, ch 6
+  esp8266_set_http_request_handler(on_http_request);
+  esp8266_server_start(80); // CIPSERVER=1,80
 
   while (1) {
   }
